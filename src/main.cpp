c3b8c4648a727a5a449952506515b317d4c24d8b
Aadil <177419930+AadilPB@users.noreply.github.com>
2026-04-02 23:29:38 -0400

    add vec3 and color headers

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,29 @@
+#include "vec3.h"
+#include "color.h"
+
 #include <iostream>
+
 
 int main() 
 {
-    std::cout << "Hello Raytracer!"; 
+    // Image
+
+    int image_width = 256;
+    int image_height = 256;
+
+    //render
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+    for (int j=0; j < image_height; j++)
+    {
+        std::clog << "\rScanline remaining: " << (image_height - j) << ' ' << std::flush;
+        for (int i = 0; i< image_width; i++)
+        {
+            auto pixel_color = color(double(i)/(image_width-1), double(j)/(image_width-1), 0);
+            write_color(std::cout, pixel_color);
+        }
+    }
+
+    std::clog << "\rDone.                 \n";
 }
 
